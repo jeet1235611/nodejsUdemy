05d847f338c36825af18708aedbd0f36a831d6ad
acoustic_jeet <js.jitendra11@gmail.com>
2018-06-15 19:38:51 +0530

    Create global.js

---
+++ global.js
@@ -0,0 +1,10 @@
+require('path');
+
+var name = "Edwin Diaz";
+var newName = name.toUpperCase(name);
+
+console.log(`newName variable is ${newName}`);
+
+console.log(__dirname);
+
+console.log(__filename);
